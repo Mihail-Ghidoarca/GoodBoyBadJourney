521bea1c28c882a1bbd9cc164a9ef2cee3c05e40
Mihail Ghidoarcă <mihailghidoarca2002@gmail.com>
2024-03-26 22:22:15 +0200

    rudimentary enemy in prototype

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,53 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
-    private bool isFacingRight = true;
-
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
-
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    private void Awake()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-
-        if (Input.GetButtonDown("Jump") && isGrounded())
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
-        }
-
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        }
-
-        Flip();
-
-        // Apply movement in FixedUpdate
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        body = GetComponent<Rigidbody2D>();
     }
 
-    private bool isGrounded()
+    private void Update()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    }
-
-    private void Flip()
-    {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
-        {
-            isFacingRight = !isFacingRight;
-            Vector3 localScale = transform.localScale;
-            localScale.x *= -1f;
-            transform.localScale = localScale;
-        }
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        if(Input.GetKeyDown(KeyCode.Space))
+            body.velocity = new Vector2(body.velocity.x, speed);
     }
 }
