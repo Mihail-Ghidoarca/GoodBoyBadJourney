3749521c4afdd04ce0d8d349a1df5352d70a2ca9
Mihail Ghidoarcă <mihailghidoarca2002@gmail.com>
2024-04-15 18:49:10 +0300

    Added player attacks, damage inflicted and damage taken from enemies

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public Animator animator;
+    public int maxHealth = 100;
+    public int health;
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        animator.SetTrigger("Hurt");
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
